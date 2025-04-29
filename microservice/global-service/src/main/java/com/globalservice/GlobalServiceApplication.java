c87fb188cf312a6a7b6e48d70bcd88578be2c382
samarthladani133 <samarthladani133@gmail.com>
2025-04-30 05:24:35 +0530

    .

---
+++ microservice/global-service/src/main/java/com/globalservice/GlobalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.globalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalServiceApplication.class, args);
+	}
+
+}
---
+++ microservice/global-service/src/test/java/com/globalservice/GlobalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
